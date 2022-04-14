cff4d5c25cbb0d219065ddc9bbe2d2f0fc40cb16
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2022-04-14 17:46:28 +0500

    enrolled/unenrolled courses added

--- src/Screens/Courses.js
+++ src/Screens/Courses.js
@@ -11,23 +11,50 @@ import { useToken } from "../hooks/useToken";
 
 const Courses = () => {
   const [listings, setListings] = useState([]);
+  const [enrolledCourse, setenrolledCourse] = useState([]);
+  const [unEnrolledCourse, setunEnrolledCourse] = useState([]);
   const navigate = useNavigate();
   const token = useToken();
-  const loadData = async () => {
-    const courseRef = await firebase.firestore().collection("courses").get();
-
-    setListings(courseRef.docs.map((e) => ({ id: e.id, data: e.data() })));
-  };
-
   useEffect(() => {
     if (!token) navigate("/sign-in", { replace: true });
+    const loadData = async () => {
+      const courseRef = await firebase.firestore().collection("courses").get();
+      setListings(
+        courseRef.docs.map((e) => {
+          return { id: e.id, data: e.data() };
+        })
+      );
+      let enrolledCourse = courseRef.docs.map((e) => {
+        if (e.data().enrolleduser.includes(token.email)) {
+          return {
+            id: e.id,
+            data: e.data(),
+          };
+        }
+      });
+      enrolledCourse = enrolledCourse.filter((e) => e !== undefined);
+      setenrolledCourse(enrolledCourse);
+
+      let unenrolledCourse = courseRef.docs.map((e) => {
+        if (!e.data().enrolleduser.includes(token.email)) {
+          return {
+            id: e.id,
+            data: e.data(),
+          };
+        }
+      });
+      unenrolledCourse = unenrolledCourse.filter((e) => e !== undefined);
+      setunEnrolledCourse(unenrolledCourse);
+    };
+
     loadData();
   }, [navigate, token]);
 
   const columnsPerRow = 3;
-
-  const getColumnsForRow = () => {
-    const items = listings?.map((data) => {
+  console.log("EnrolledCouse", enrolledCourse);
+  console.log("unEnrolledCourse", unEnrolledCourse);
+  const getColumnsForRow = (courses) => {
+    const items = courses?.map((data) => {
       return (
         <Col key={data.id}>
           <Card
@@ -54,10 +81,21 @@ const Courses = () => {
       />
       <BreadcrumbPage />
       <Container>
-        {listings && (
-          <Row xs={1} md={columnsPerRow}>
-            {getColumnsForRow()}
-          </Row>
+        {enrolledCourse.length > 0 && (
+          <>
+            <h1>Your Courses</h1>
+            <Row xs={1} md={columnsPerRow}>
+              {getColumnsForRow(enrolledCourse)}
+            </Row>
+          </>
+        )}
+        {unEnrolledCourse.length > 0 && (
+          <>
+            <h1 className="mt-5">Other Courses</h1>
+            <Row xs={1} md={columnsPerRow}>
+              {getColumnsForRow(unEnrolledCourse)}
+            </Row>
+          </>
         )}
       </Container>
     </>
