9fcc1f87cc773908eb50881f6c4d766eaa3031a4
Abdul Raheem <61801382+abdulraheem286@users.noreply.github.com>
2022-05-23 00:30:19 +0500

    header updated

--- src/components/ELearning/Header.js
+++ src/components/ELearning/Header.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-const Header = ({ pageName, bgImage, color }) => {
+const Header = ({ pageName, bgImage }) => {
   return (
     <>
       <div
         style={{
           backgroundColor: "#F5F5F5",
-          height: 300,
+          height: 400,
           backgroundImage: `url(${bgImage})`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          color,
+          position: "relative",
         }}
       >
-        <Container style={{ paddingTop: 100 }}>
+        <div
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+
+        <Container
+          style={{ paddingTop: 100, position: "relative", zIndex: "100" }}
+        >
           <h1
             style={{
-              textDecoration: "underline",
               textUnderlineOffset: "8px",
               fontSize: "48px",
-              fontFamily: "'Raleway', sans-serif",
+              fontFamily: "'Poppins', sans-serif",
+              color: "white",
             }}
           >
             {pageName}
